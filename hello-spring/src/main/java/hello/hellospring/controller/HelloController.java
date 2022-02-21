446b8cfc9d5a4ceab9165e2707ed314d3e5ab9b7
이혜연 <ihyeyeon@ihyeyeon-ui-MacBookAir.local>
2022-02-21 23:17:54 +0900

    컨트롤러와 thymeleaf를 이용한 렌더링

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,4 +1,18 @@
 package hello.hellospring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
+
+    //메서드가 hello라고 들어오면 이걸 호출을 해준다
+    @GetMapping("hello")
+    public String hello(Model model) {
+        //attributeName와 attributeValue는 부가 설명 같은 것 !
+        // data라는 이름의 hello 라는 값을 가진걸 넘기고, hello를 return 한다.
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
 }
